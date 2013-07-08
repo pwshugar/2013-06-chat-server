c403c8a4f58f955e447676aa71d2842fbd9dd9d6
Hack Reactor Students <github@hackreactor.com>
2013-07-08 16:17:46 -0700

    chat client connects to the node server

--- request-handler.js
+++ request-handler.js
@@ -3,7 +3,32 @@
  * basic-server.js.  So you must figure out how to export the function
  * from this file and include it in basic-server.js. Check out the
  * node module documentation at http://nodejs.org/api/modules.html. */
+// var url = require("url");
 
-var handleRequest = function(request, response) {
+var defaultCorsHeaders = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10 // Seconds.
+};
 
+exports.handleRequest = function(request, response) {
+  // var pathname = url.parse(request.url).pathname;
+  console.log("Serving request type " + request.method + " for url " + request.url);
+
+  var statusCode = 200;
+
+  var headers = defaultCorsHeaders;
+  headers['Content-Type'] = "text/plain";
+  response.writeHead(statusCode, headers);
+
+  var data = {
+    results: [{
+      username: 'Sonomi',
+      text: 'Hello'
+    }]
+  };
+
+
+  response.end(JSON.stringify(data));
 };
